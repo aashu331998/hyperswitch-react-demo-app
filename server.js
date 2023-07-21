383c32bffcd4c065278e6bc1180870647baeaba3
shivam <shivam.shashank@juspay.in>
2023-07-21 14:50:26 +0530

    codesandbox fixes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const { resolve } = require("path");
 
 // Replace if using a different env file or config
